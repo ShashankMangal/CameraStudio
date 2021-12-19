3155b77018ed8434d2eed0476e1bcf22752aa918
ShashankMangal <shashankmangal10@gmail.com>
2021-12-19 17:32:18 +0530

    Test Ads Implemented & Sharing and settings remaining & splash screen

--- app/src/main/java/com/example/camerastudio/Screens/MainScreen.java
+++ app/src/main/java/com/example/camerastudio/Screens/MainScreen.java
@@ -2,26 +2,51 @@ package com.example.camerastudio.Screens;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.ImageView;
 
+import com.dsphotoeditor.sdk.activity.DsPhotoEditorActivity;
+import com.dsphotoeditor.sdk.utils.DsPhotoEditorConstants;
 import com.example.camerastudio.R;
 import com.example.camerastudio.databinding.ActivityMainScreenBinding;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.ads.initialization.InitializationStatus;
+import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
+
+import java.io.ByteArrayOutputStream;
 
 public class MainScreen extends AppCompatActivity {
 
 
     private ActivityMainScreenBinding binding;
     private int IMAGE_REQUEST_CODE = 45;
+    private int CAMERA_REQUEST_CODE = 14;
+    private int RESULT_CODE = 200;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityMainScreenBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        MobileAds.initialize(this, new OnInitializationCompleteListener() {
+            @Override
+            public void onInitializationComplete(InitializationStatus initializationStatus) {
+            }
+        });
+
+        AdRequest adRequest = new AdRequest.Builder().build();
+        binding.adView.loadAd(adRequest);
 
         getSupportActionBar().hide();
 
@@ -32,6 +57,20 @@ public class MainScreen extends AppCompatActivity {
                 intent.setAction(Intent.ACTION_GET_CONTENT);
                 intent.setType("image/*");
                 startActivityForResult(intent, IMAGE_REQUEST_CODE);
+            }
+        });
+
+        binding.cameraButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(ActivityCompat.checkSelfPermission(MainScreen.this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED)
+                {
+                    ActivityCompat.requestPermissions(MainScreen.this, new String[] {Manifest.permission.CAMERA},2);
+                }else
+                {
+                    Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                    startActivityForResult(cameraIntent, CAMERA_REQUEST_CODE);
+                }
             }
         });
 
@@ -46,8 +85,40 @@ public class MainScreen extends AppCompatActivity {
         {
             if(data.getData() != null)
             {
-
+                Uri filePath = data.getData();
+                Intent dsPhotoEditorIntent = new Intent(this, DsPhotoEditorActivity.class);
+                dsPhotoEditorIntent.setData(filePath);
+                dsPhotoEditorIntent.putExtra(DsPhotoEditorConstants.DS_PHOTO_EDITOR_OUTPUT_DIRECTORY, "CameraStudio");
+                int[] toolsToHide = {DsPhotoEditorActivity.TOOL_ORIENTATION, DsPhotoEditorActivity.TOOL_CROP};
+                dsPhotoEditorIntent.putExtra(DsPhotoEditorConstants.DS_PHOTO_EDITOR_TOOLS_TO_HIDE, toolsToHide);
+                startActivityForResult(dsPhotoEditorIntent, RESULT_CODE);
             }
         }
+
+        if(requestCode == RESULT_CODE)
+        {
+            Intent intent = new Intent(MainScreen.this , ResultScreen.class);
+            intent.setData(data.getData());
+            startActivity(intent);
+        }
+
+        if(requestCode == CAMERA_REQUEST_CODE)
+        {
+            Bitmap photo = (Bitmap) data.getExtras().get("data");
+            Uri uri = getImageUri(photo);
+            Intent dsPhotoEditorIntent = new Intent(this, DsPhotoEditorActivity.class);
+            dsPhotoEditorIntent.setData(uri);
+            dsPhotoEditorIntent.putExtra(DsPhotoEditorConstants.DS_PHOTO_EDITOR_OUTPUT_DIRECTORY, "CameraStudio");
+            int[] toolsToHide = {DsPhotoEditorActivity.TOOL_ORIENTATION, DsPhotoEditorActivity.TOOL_CROP};
+            dsPhotoEditorIntent.putExtra(DsPhotoEditorConstants.DS_PHOTO_EDITOR_TOOLS_TO_HIDE, toolsToHide);
+            startActivityForResult(dsPhotoEditorIntent, RESULT_CODE);
+        }
+    }
+    public Uri getImageUri(Bitmap bitmap)
+    {
+        ByteArrayOutputStream arrayOutputStream = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.JPEG,100,arrayOutputStream);
+        String path = MediaStore.Images.Media.insertImage(getContentResolver(), bitmap, "Title",null);
+        return Uri.parse(path);
     }
 }
--- app/src/main/java/com/example/camerastudio/Screens/ResultScreen.java
+++ app/src/main/java/com/example/camerastudio/Screens/ResultScreen.java
@@ -1,16 +1,98 @@
 package com.example.camerastudio.Screens;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
 
 import com.example.camerastudio.R;
+import com.example.camerastudio.databinding.ActivityResultScreenBinding;
+import com.google.android.gms.ads.AdError;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.FullScreenContentCallback;
+import com.google.android.gms.ads.LoadAdError;
+import com.google.android.gms.ads.interstitial.InterstitialAd;
+import com.google.android.gms.ads.interstitial.InterstitialAdLoadCallback;
 
 public class ResultScreen extends AppCompatActivity {
+
+    private ActivityResultScreenBinding binding;
+    private InterstitialAd mInterstitialAd;
+    private String TAG = "Ad Status";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_result_screen);
+        binding = ActivityResultScreenBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        getSupportActionBar().hide();
+
+        binding.finalImage.setImageURI(getIntent().getData());
+        binding.homeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(ResultScreen.this , MainScreen.class));
+                Toast.makeText(getApplicationContext(), "Image Saved..", Toast.LENGTH_SHORT).show();
+                finish();
+            }
+        });
+
+        AdRequest adRequest = new AdRequest.Builder().build();
+
+
+        InterstitialAd.load(this,"ca-app-pub-3940256099942544/1033173712", adRequest,
+                new InterstitialAdLoadCallback() {
+                    @Override
+                    public void onAdLoaded(@NonNull InterstitialAd interstitialAd) {
+                        // The mInterstitialAd reference will be null until
+                        // an ad is loaded.
+                        mInterstitialAd = interstitialAd;
+                        mInterstitialAd.show(ResultScreen.this);
+                        mInterstitialAd.setFullScreenContentCallback(new FullScreenContentCallback() {
+                            @Override
+                            public void onAdFailedToShowFullScreenContent(@NonNull AdError adError) {
+                                super.onAdFailedToShowFullScreenContent(adError);
+                                Toast.makeText(ResultScreen.this, "Ads Failed to Load.", Toast.LENGTH_SHORT).show();
+                            }
+
+                            @Override
+                            public void onAdShowedFullScreenContent() {
+                                super.onAdShowedFullScreenContent();
+                            }
+
+                            @Override
+                            public void onAdDismissedFullScreenContent() {
+                                super.onAdDismissedFullScreenContent();
+                            }
+
+                            @Override
+                            public void onAdImpression() {
+                                super.onAdImpression();
+
+                            }
+
+                            @Override
+                            public void onAdClicked() {
+                                super.onAdClicked();
+                                Toast.makeText(ResultScreen.this, "Ad Clicked.", Toast.LENGTH_SHORT).show();
+                            }
+                        });
+                        Log.i(TAG, "onAdLoaded");
+
+                    }
+
+                    @Override
+                    public void onAdFailedToLoad(@NonNull LoadAdError loadAdError) {
+                        // Handle the error
+                        Log.i(TAG, loadAdError.getMessage());
+                        mInterstitialAd = null;
+                    }
+                });
     }
 }

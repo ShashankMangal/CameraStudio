5e6267908518cea333e5e64500a3a1ff1e6e3249
ShashankMangal <shashankmangal10@gmail.com>
2021-12-19 14:08:35 +0530

    MainScreen XMl, ResultScreen XML , DS Editor added

---
+++ app/src/main/java/com/example/camerastudio/Screens/MainScreen.java
@@ -0,0 +1,53 @@
+package com.example.camerastudio.Screens;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.example.camerastudio.R;
+import com.example.camerastudio.databinding.ActivityMainScreenBinding;
+
+public class MainScreen extends AppCompatActivity {
+
+
+    private ActivityMainScreenBinding binding;
+    private int IMAGE_REQUEST_CODE = 45;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainScreenBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        getSupportActionBar().hide();
+
+        binding.editButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                intent.setType("image/*");
+                startActivityForResult(intent, IMAGE_REQUEST_CODE);
+            }
+        });
+
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == IMAGE_REQUEST_CODE)
+        {
+            if(data.getData() != null)
+            {
+
+            }
+        }
+    }
+}

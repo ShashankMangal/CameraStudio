6d5e57570d865c0d71dbc7f29a7d8f5fff748b59
ShashankMangal <shashankmangal10@gmail.com>
2022-01-03 23:05:21 +0530

    Compressor working but rotating Image

--- app/src/main/java/com/sharkBytesLab/camerastudio/Screens/CompressScreen.java
+++ app/src/main/java/com/sharkBytesLab/camerastudio/Screens/CompressScreen.java
@@ -4,24 +4,26 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.Manifest;
+import android.content.ContentResolver;
+import android.content.ContentValues;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
 import android.text.format.Formatter;
+import android.util.Log;
 import android.view.View;
 import android.widget.SeekBar;
 import android.widget.Toast;
-
 import com.karumi.dexter.Dexter;
 import com.karumi.dexter.MultiplePermissionsReport;
 import com.karumi.dexter.PermissionToken;
 import com.karumi.dexter.listener.PermissionRequest;
 import com.karumi.dexter.listener.multi.MultiplePermissionsListener;
-import com.sharkBytesLab.camerastudio.KotlinCode.CompressActivity;
 import com.sharkBytesLab.camerastudio.R;
 import com.sharkBytesLab.camerastudio.databinding.ActivityCompressScreenBinding;
 
@@ -30,15 +32,16 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.List;
-
-import id.zelory.compressor.Compressor;
+import java.util.Objects;
 
 public class CompressScreen extends AppCompatActivity {
 
     private ActivityCompressScreenBinding binding;
     private File originalImage, compressedImage;
     private static String filePath;
+    Bitmap selectedImage, compressedImg;
     File path = new File(Environment.getExternalStorageDirectory().getAbsolutePath()+"/Camera Studio Compress");
 
     public static final int RESULT_IMAGE = 1;
@@ -63,6 +66,7 @@ public class CompressScreen extends AppCompatActivity {
             public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
                 binding.textQuality.setText("Quality : "+i);
                 seekBar.setMax(100);
+
             }
 
             @Override
@@ -91,15 +95,16 @@ public class CompressScreen extends AppCompatActivity {
                 int height = Integer.parseInt(binding.txtHeight.getText().toString());
 
                 try {
-                    //CompressorCode compress = new CompressorCode(getApplicationContext(), originalImage, filePath);
-//
-//                    compressedImage = new Compressor.INSTANCE.compress()
-//                            .setMaxWidth(width)
-//                            .setMaxHeight(height)
-//                            .setQuality(quality)
-//                            .setCompressFormat(Bitmap.CompressFormat.JPEG)
-//                            .setDestinationDirectoryPath(filePath)
-//                            .compressToFile(originalImage);
+
+
+                    compressedImg = ShrinkBitmap(originalImage.toString(), width, height);
+                    binding.imgCompressed.setImageBitmap(compressedImg);
+                    Uri location = saveImage(compressedImg);
+                    compressedImage = new File(location.getPath());
+                    Log.d("Comp Img Loc: ",location.getPath().replace("raw/",""));
+                    binding.txtCompressed.setText("Size : "+ Formatter.formatShortFileSize(CompressScreen.this, compressedImage.length()));
+
+
 
 
                 } catch (Exception e) {
@@ -150,9 +155,10 @@ public class CompressScreen extends AppCompatActivity {
             final Uri imageUri = data.getData();
             try {
                 final InputStream imageStream = getContentResolver().openInputStream(imageUri);
-                final Bitmap selectedImage = BitmapFactory.decodeStream(imageStream);
+                selectedImage = BitmapFactory.decodeStream(imageStream);
                 binding.imgOriginal.setImageBitmap(selectedImage);
                 originalImage = new File(imageUri.getPath().replace("raw/",""));
+                Log.d("Orig Img Loc: ",imageUri.getPath());
                 binding.txtOriginal.setText("Size : "+ Formatter.formatShortFileSize(this, originalImage.length()));
             } catch (FileNotFoundException e) {
                 e.printStackTrace();
@@ -163,4 +169,59 @@ public class CompressScreen extends AppCompatActivity {
             Toast.makeText(getApplicationContext(), "No Image Selected!", Toast.LENGTH_SHORT).show();
         }
     }
+
+    public Bitmap ShrinkBitmap(String file, int width, int height)
+    {
+        BitmapFactory.Options bmpFactoryOptions = new BitmapFactory.Options();
+        bmpFactoryOptions.inJustDecodeBounds = true;
+        Bitmap bit = BitmapFactory.decodeFile(file, bmpFactoryOptions);
+
+        int heightRatio = (int) Math.ceil(bmpFactoryOptions.outHeight / (float) height);
+        int widthRatio = (int) Math.ceil(bmpFactoryOptions.outWidth / (float) width);
+
+        if(heightRatio > 1 || widthRatio > 1)
+        {
+            if(heightRatio > widthRatio)
+            {
+                bmpFactoryOptions.inSampleSize = heightRatio;
+            }
+            else
+            {
+                bmpFactoryOptions.inSampleSize = widthRatio;
+            }
+        }
+
+        bmpFactoryOptions.inJustDecodeBounds = false;
+        bit = BitmapFactory.decodeFile(file, bmpFactoryOptions);
+        return bit;
+    }
+
+    private Uri saveImage(Bitmap bitmap)
+    {
+        OutputStream fos;
+        Uri imageUri = null;
+        try
+        {
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q)
+        {
+            ContentResolver resolver = getContentResolver();
+            ContentValues contentValues = new ContentValues();
+            contentValues.put(MediaStore.MediaColumns.DISPLAY_NAME, "Camera Studio Compress"+".jpg");
+            contentValues.put(MediaStore.MediaColumns.MIME_TYPE, "image/jpg");
+            imageUri = resolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, contentValues);
+            fos = resolver.openOutputStream(Objects.requireNonNull(imageUri));
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, fos);
+            Objects.requireNonNull(fos);
+            Log.d("fos : ",fos.toString());
+            Log.d("imageUri : ",imageUri.getPath());
+            Log.d("contentValues : ",contentValues.toString());
+
+        }
+        }catch(Exception e)
+        {
+            Log.d("Save Image Error : ",e.getMessage());
+        }
+        return imageUri;
+    }
+
 }

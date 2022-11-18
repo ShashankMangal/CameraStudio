8e3abafa434c608ede606bb2e17ccb6b6e53897b
shashankmangal10@gmail.com <Sunil@1234>
2022-11-18 19:39:29 +0530

    new update ready

--- app/src/main/java/com/sharkBytesLab/camerastudio/Screens/MainScreen.java
+++ app/src/main/java/com/sharkBytesLab/camerastudio/Screens/MainScreen.java
@@ -1,9 +1,5 @@
 package com.sharkBytesLab.camerastudio.Screens;
 
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-
 import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
@@ -12,18 +8,34 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
 import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.res.ResourcesCompat;
+
+import com.applovin.mediation.MaxAd;
+import com.applovin.mediation.MaxError;
+import com.applovin.mediation.MaxReward;
+import com.applovin.mediation.MaxRewardedAdListener;
+import com.applovin.mediation.ads.MaxRewardedAd;
 import com.applovin.sdk.AppLovinSdk;
 import com.applovin.sdk.AppLovinSdkConfiguration;
 import com.dsphotoeditor.sdk.activity.DsPhotoEditorActivity;
 import com.dsphotoeditor.sdk.utils.DsPhotoEditorConstants;
+import com.sharkBytesLab.camerastudio.R;
 import com.sharkBytesLab.camerastudio.databinding.ActivityMainScreenBinding;
 
 import java.io.ByteArrayOutputStream;
+import java.util.concurrent.TimeUnit;
 
-public class MainScreen extends AppCompatActivity {
+import www.sanju.motiontoast.MotionToast;
+import www.sanju.motiontoast.MotionToastStyle;
+
+public class MainScreen extends AppCompatActivity implements MaxRewardedAdListener {
 
 
     private ActivityMainScreenBinding binding;
@@ -31,6 +43,9 @@ public class MainScreen extends AppCompatActivity {
     private int CAMERA_REQUEST_CODE = 14;
     private int RESULT_CODE = 200;
     boolean isPressed = false;
+    private MaxRewardedAd rewardedAd;
+    private int retryAttempt;
+    private final String TAG = "Rewarded Ads";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,7 +57,6 @@ public class MainScreen extends AppCompatActivity {
 
         getSupportActionBar().hide();
 
-        // Please make sure to set the mediation provider value to "max" to ensure proper functionality
         AppLovinSdk.getInstance( this ).setMediationProvider( "max" );
         AppLovinSdk.initializeSdk( this, new AppLovinSdk.SdkInitializationListener() {
             @Override
@@ -52,6 +66,9 @@ public class MainScreen extends AppCompatActivity {
             }
         } );
 
+        rewardedAd = MaxRewardedAd.getInstance("e061615381f3637c", this);
+        rewardedAd.setListener(this);
+        rewardedAd.loadAd();
         binding.applovinAd.loadAd();
 
         binding.compressButton.setOnClickListener(new View.OnClickListener() {
@@ -77,6 +94,12 @@ public class MainScreen extends AppCompatActivity {
             @Override
             public void onClick(View view) {
 
+                if (rewardedAd.isReady()) {
+                    rewardedAd.showAd();
+                    Log.i(TAG, "Show");
+                } else {
+                    showToast("Getting Ads...");
+                }
                 startActivity(new Intent(MainScreen.this, MenuScreen.class));
                 finish();
             }
@@ -193,4 +216,82 @@ public class MainScreen extends AppCompatActivity {
         new Handler().postDelayed(runnable,2000);
 
     }
+
+    @Override
+    public void onAdLoaded(final MaxAd maxAd) {
+        // Rewarded ad is ready to be shown. rewardedAd.isReady() will now return 'true'
+
+        // Reset retry attempt
+        Log.i(TAG, "Loaded");
+        showToast("Loading..");
+        retryAttempt = 0;
+    }
+
+    @Override
+    public void onAdLoadFailed(final String adUnitId, final MaxError error) {
+        // Rewarded ad failed to load
+        // We recommend retrying with exponentially higher delays up to a maximum delay (in this case 64 seconds)
+        Log.i(TAG, "Failed");
+        retryAttempt++;
+        long delayMillis = TimeUnit.SECONDS.toMillis((long) Math.pow(2, Math.min(6, retryAttempt)));
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                rewardedAd.loadAd();
+            }
+        }, delayMillis);
+    }
+
+    @Override
+    public void onAdDisplayFailed(final MaxAd maxAd, final MaxError error) {
+        Log.i(TAG, "Failed");
+        // Rewarded ad failed to display. We recommend loading the next ad
+        rewardedAd.loadAd();
+    }
+
+    @Override
+    public void onAdDisplayed(final MaxAd maxAd) {
+        Log.i(TAG, "Displayed");
+    }
+
+    @Override
+    public void onAdClicked(final MaxAd maxAd) {
+    }
+
+    @Override
+    public void onAdHidden(final MaxAd maxAd) {
+        Log.i(TAG, "Hidden");
+        // rewarded ad is hidden. Pre-load the next ad
+        rewardedAd.loadAd();
+    }
+
+    @Override
+    public void onRewardedVideoStarted(final MaxAd maxAd) {
+    }
+
+    @Override
+    public void onRewardedVideoCompleted(final MaxAd maxAd) {
+        Log.i(TAG, "Completed");
+
+        Intent intent = new Intent(getApplicationContext(), MenuScreen.class);
+        startActivity(intent);
+    }
+
+    @Override
+    public void onUserRewarded(final MaxAd maxAd, final MaxReward maxReward) {
+        // Rewarded ad was displayed and user should receive the reward
+    }
+
+    private void showToast(String s) {
+        MotionToast.Companion.createColorToast(this,
+                s,
+                "TRY AGAIN AFTER SOME TIME",
+                MotionToastStyle.WARNING,
+                MotionToast.GRAVITY_BOTTOM,
+                10000,
+                ResourcesCompat.getFont(this, R.font.helvetica_regular));
+
+    }
+
 }

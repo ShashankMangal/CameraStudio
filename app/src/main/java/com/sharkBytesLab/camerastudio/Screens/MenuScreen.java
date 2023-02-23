46cc2c894bd6482c769f2f665ee46cb73f74f4ca
shashankmangal10@gmail.com <Sunil@1234>
2023-02-23 15:36:04 +0530

    new update ready

--- app/src/main/java/com/sharkBytesLab/camerastudio/Screens/MenuScreen.java
+++ app/src/main/java/com/sharkBytesLab/camerastudio/Screens/MenuScreen.java
@@ -6,18 +6,30 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
+import com.applovin.mediation.MaxAd;
+import com.applovin.mediation.MaxAdListener;
+import com.applovin.mediation.MaxError;
+import com.applovin.mediation.ads.MaxAdView;
+import com.applovin.mediation.ads.MaxInterstitialAd;
 import com.sharkBytesLab.camerastudio.BuildConfig;
 import com.sharkBytesLab.camerastudio.PrivacyPolicy.PrivacyPolicyActivity;
 import com.sharkBytesLab.camerastudio.PrivacyPolicy.TermsActivity;
 import com.sharkBytesLab.camerastudio.databinding.ActivityMenuScreenBinding;
 
-public class MenuScreen extends AppCompatActivity {
+import java.util.concurrent.TimeUnit;
+
+public class MenuScreen extends AppCompatActivity implements MaxAdListener {
 
     private ActivityMenuScreenBinding binding;
+    private MaxInterstitialAd interstitialAd;
+
+    private MaxAdView adView;
+    private int retryAttempt;
 
 
     @Override
@@ -28,8 +40,22 @@ public class MenuScreen extends AppCompatActivity {
 
         getSupportActionBar().hide();
 
+        binding.applovinAdMenu.loadAd();
+        interstitialAd = new MaxInterstitialAd( "81e3415423b47939", this );
+        interstitialAd.setListener( this );
+        interstitialAd.loadAd();
 
+        binding.supportUs.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
+                if ( interstitialAd.isReady() )
+                {
+                    interstitialAd.showAd();
+                }
+
+            }
+        });
 
         binding.homeButtonMenu.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -128,4 +154,53 @@ public class MenuScreen extends AppCompatActivity {
         startActivity(new Intent(MenuScreen.this, MainScreen.class));
         finish();
     }
+
+    @Override
+    public void onAdLoaded(final MaxAd maxAd)
+    {
+        // Interstitial ad is ready to be shown. interstitialAd.isReady() will now return 'true'
+        // Reset retry attempt
+        retryAttempt = 0;
+    }
+
+    @Override
+    public void onAdLoadFailed(final String adUnitId, final MaxError error)
+    {
+        // Interstitial ad failed to load
+        // AppLovin recommends that you retry with exponentially higher delays up to a maximum delay (in this case 64 seconds)
+
+        retryAttempt++;
+        long delayMillis = TimeUnit.SECONDS.toMillis( (long) Math.pow( 2, Math.min( 6, retryAttempt ) ) );
+
+        new Handler().postDelayed(new Runnable()
+        {
+            @Override
+            public void run()
+            {
+                interstitialAd.loadAd();
+            }
+        }, delayMillis );
+    }
+
+    @Override
+    public void onAdDisplayFailed(final MaxAd maxAd, final MaxError error)
+    {
+        // Interstitial ad failed to display. AppLovin recommends that you load the next ad.
+        interstitialAd.loadAd();
+    }
+
+    @Override
+    public void onAdDisplayed(final MaxAd maxAd) {}
+
+    @Override
+    public void onAdClicked(final MaxAd maxAd) {}
+
+    @Override
+    public void onAdHidden(final MaxAd maxAd)
+    {
+        // Interstitial ad is hidden. Pre-load the next ad
+        interstitialAd.loadAd();
+    }
+
+
 }
